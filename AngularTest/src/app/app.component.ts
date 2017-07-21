cd618af6dfe3a3755ded75d68189e814fe9113b1
Faisal <faisalsharif1@gmail.com>
2017-07-21 22:56:52 +0500

    Initial Structure

--- AngularTest/src/app/app.component.ts
+++ AngularTest/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { testComponent } from './test.component';
 
 @Component({
   selector: 'my-app',
   template: 
 `
     <h1>Name Of The Developer Is {{name}}</h1>
-    <h2>Father Name Of The Developer Iss {{fatherName}}</h2>
-`,
+    <test-app></test-app>
+`
 })
 
 export class AppComponent
---
+++ AngularTest/src/app/test.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'test-app',
+    template:
+    `
+    <h2>Name Of The Developer Is {{name}}</h2>
+`
+})
+
+export class testComponent {
+    name = 'Shah Fahad';
+}
